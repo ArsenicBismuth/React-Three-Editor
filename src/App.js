d4301e5833230ed2445efe6dac7279fa14efd82e
Arsenic Bismuth <d.faris323@gmail.com>
2023-11-28 12:15:42 +0700

    Initial model samples

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import './App.css';
 
 import React, { Suspense, useRef } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import * as THREE from 'three'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Stage } from '@react-three/drei'
 
-import { Model } from './Model'
+import { ImportGLTF, ImportFBX } from './Model'
+
+THREE.ColorManagement.enabled = true
+
+function Scene() {
+  const ref = useRef()
+  return (
+    <Stage controls={ref} preset="rembrandt" intensity={1}  environment="city">
+    false
+      <ImportGLTF />
+      <ImportFBX />
+    false
+    </Stage>
+  )
+}
 
 function App() {
   const ref = useRef()
@@ -13,18 +29,12 @@ function App() {
     <div id="canvas-container">
       <Canvas shadows dpr={[1, 2]} camera={{ fov: 50 }}>
         <Suspense fallback={null}>
-          <Stage controls={ref} preset="rembrandt" intensity={1}  environment="city">
-          false
-            <Model />
-          false
-          </Stage>
+          <Scene/>
         </Suspense>
         <OrbitControls ref={ref} />
       </Canvas>
     </div>
   )
 }
-
-createRoot(document.getElementById('root')).render(<App />)
 
 export default App;
