53528de303a4d723edbb50995fbd7281ef703cd3
Arsenic Bismuth <d.faris323@gmail.com>
2023-11-28 20:09:27 +0700

    More params

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import './App.css';
 
 import React, { Suspense, useRef } from 'react'
 
-import * as dat from 'dat.gui';
-
 import * as THREE from 'three'
 import { Canvas, useThree } from '@react-three/fiber'
 import { OrbitControls, Stage } from '@react-three/drei'
@@ -12,24 +10,13 @@ import { ImportGLTF, ImportFBX, getSelection } from './Model'
 
 function Scene({orbitRef}) {
   return (
-    <Stage controls={orbitRef} preset="rembrandt" intensity={1}  environment="city">
+    <Stage controls={orbitRef} preset="rembrandt" intensity={1}  environment="forest">
     false
       <directionalLight position={[10, 10, 10]} intensity={2} />
       <ImportFBX />
     false
     </Stage>
   )
-}
-
-function UserInterface() {
-  const gui = new dat.GUI()
-  gui.addColor({ color: '#000000' }, 'color')
-    .listen()
-    .onChange((e) => {
-      getSelection().traverse((o) => {
-        if (o.isMesh) o.material.color.setStyle(e);
-      })
-    })
 }
 
 function App() {
@@ -40,10 +27,9 @@ function App() {
     <div id="canvas-container">
       <Canvas shadows dpr={[1, 2]} camera={{ fov: 50 }}>
         <Suspense fallback={null}>
-          <Scene orbitRef={orbitRef}/>
+          <Scene orbitRef={orbitRef} />
         </Suspense>
         <OrbitControls ref={orbitRef} />
-        <UserInterface />
       </Canvas>
     </div>
   )
--- src/Model.js
+++ src/Model.js
@@ -1,6 +1,9 @@
 import React, { useRef } from "react";
 import { useGLTF, useFBX } from "@react-three/drei";
 
+import * as dat from 'dat.gui';
+
+// Importer
 export function ImportGLTF(props) {
   const { nodes, materials } = useGLTF("/suzanne.gltf")
   return (
@@ -20,16 +23,61 @@ export function ImportFBX(props) {
   const fbx = useFBX("/char.fbx")
   Selected = useRef()
 
+  setTimeout(() => {
+    UserInterface()
+  }, 2000)
+
   return (
     <primitive object={fbx}
       {...props}
       scale={0.01}
-      ref={Selected}
-    />
+      ref={Selected}>
+    </primitive>
   )
 }
 
+// Selection
 let Selected = null
 export function getSelection() {
   return Selected.current
-};
+}
+
+function getMaterial() {
+  const obj = getSelection()
+  if (!obj) return null
+
+  let mat = null
+  obj.traverse((o) => {
+    if (!o.isMesh) return
+    mat = o.material
+  })
+
+  return mat
+}
+
+// GUI
+function handleColorChange( color ) {
+  return (value) => {
+    if ( typeof value === 'string' ) {
+      value = value.replace( '#', '0x' );
+    }
+    color.setHex(value)
+  }
+}
+
+function UserInterface() {
+  const gui = new dat.GUI()
+  const mat = getMaterial()
+  console.log(mat)
+
+  gui.addColor( {"color": mat.color.getHex()}, 'color').onChange( handleColorChange( mat.color ) )
+  gui.addColor( {"emissive": mat.emissive.getHex()}, 'emissive').onChange( handleColorChange( mat.emissive ) )
+  gui.addColor( {"specular": mat.specular.getHex()}, 'specular').onChange( handleColorChange( mat.specular ) )
+  
+  gui.add( mat, 'wireframe' )
+  gui.add( mat, 'transparent' ).onChange( () => {mat.needsUpdate = true} )
+  gui.add( mat, 'opacity', 0, 1 ).step( 0.01 )
+  gui.add( mat, 'reflectivity', 0, 1 )
+  gui.add( mat, 'shininess', 0, 100 )
+  gui.add( mat, 'refractionRatio', 0, 1 )
+}
